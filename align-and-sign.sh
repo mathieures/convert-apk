2a204418fbb30c99d0915899541f85ba1b0eeebe
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-09 19:24:06 +0200

    Prototyped bash script to align and sign apk

---
+++ align-and-sign.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if (( $# != 2 )); then
+    echo "Usage : $0 <src.apk> <dst.apk>"
+    exit
+fi
+
+src=$1
+temp=temp_$dst
+dst=$2
+
+keyname=temp_key.jks
+
+
+echo "creating key; you can enter dummy credentials"
+keytool -genkey -v -keystore $keyname -keyalg RSA -keysize 2048 -validity 10000 -alias my-alias
+
+# TODO: fill info automatically
+
+echo "aligning $src into $temp"
+zipalign -v -p 4 $src $temp
+
+echo "signing $temp into $dst"
+apksigner sign --ks $keyname --out $dst $temp
+
+# echo "deleting key and $temp"
+# rm -f $keyname
+# rm -f $temp
+
+
+echo 'done'
