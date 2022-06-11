1ba136b74262bbbc46dc62196efe96df83fe380d
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-11 23:51:16 +0200

    Added option to use tar.exe: there is no compression, but it is faster

--- uncompress-resources.bat
+++ uncompress-resources.bat
@@ -1,12 +1,19 @@
 @echo off
-setlocal
+
+rem If -t is present
+set "use_tar="
+if "%1" == "-t" (
+    set use_tar=true
+    shift
+)
 
 if "%2"=="" goto usage
 if not "%3"=="" goto usage
 goto begin
 
 :usage
-echo Usage : %0 ^<src.apk^> ^<dst.apk^>
+echo Usage : %~nx0 [-t] ^<src.apk^> ^<dst.apk^>
+echo    -t : use tar instead of powershell (no compression)
 exit /b
 
 :begin
@@ -15,23 +22,41 @@ set unzipped=%src%.unzipped
 set dst=%2
 set dst_zip=%dst%.zip
 
+
 echo unzipping into %unzipped%
 md %unzipped% 2>NUL
 tar -P -xf %src% -C %unzipped%
 
 
-echo zipping back into %dst% (slow process)
+echo zipping back into %dst%
+if "%use_tar%" == "true" (call :use_tar) else call :use_ps
+goto renaming
+
+:use_tar
+echo using tar
+tar -P -acf %dst_zip% --options compression-level=0 -C %unzipped% *
+exit /b
+
+:use_ps
 rem If PowerShell 7 is available, use it
 where pwsh.exe >NUL 2>&1
 if %errorlevel% equ 0 (
+    echo using pwsh.exe
     set ps=pwsh.exe
 ) else (
+    echo using powershell.exe
     set ps=powershell.exe
 )
-
 rem Compress everything but resources.arsc
-call %ps% -nol -noni -nop -c Compress-Archive (Get-ChildItem %unzipped% -Exclude 'resources.arsc') %dst_zip% ; Compress-Archive %unzipped%\resources.arsc -Update %dst_zip% -CompressionLevel NoCompression
-rename %dst_zip% %dst%
+call %ps% -nol -noni -nop -c Compress-Archive (Get-ChildItem %unzipped% -Exclude 'resources.arsc') %dst_zip% -Force ; Compress-Archive %unzipped%\resources.arsc -Update %dst_zip% -CompressionLevel NoCompression
+exit /b
+
+
+:renaming
+rem Overwrite destination if it exists
+if exist %dst% del /q %dst%
+rename %dst_zip% %~nx2
+
 
 echo deleting %unzipped%
 rmdir /s /q %unzipped%
