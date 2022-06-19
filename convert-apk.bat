e977f7c5fe98ee337f808beba9c2e22beb461f5c
mathieures <71087433+mathieures@users.noreply.github.com>
2022-06-19 22:39:15 +0200

    Added support for the -t flag

--- convert-apk.bat
+++ convert-apk.bat
@@ -1,11 +1,19 @@
 @echo off
 
+rem If -t is present
+set "use_tar="
+if "%1" == "-t" (
+    set use_tar=true
+    shift
+)
+
 if "%2"=="" goto usage
 if not "%3"=="" goto usage
 goto begin
 
 :usage
-echo Usage : %0 ^<src.apk^> ^<dst.apk^>
+echo Usage : %0 [-t] ^<src.apk^> ^<dst.apk^>
+echo    -t : use tar instead of powershell (no compression)
 exit /b
 
 :begin
@@ -13,5 +21,9 @@ set src=%1
 set unzipped=%src%.unzipped
 set dst=%2
 
-call uncompress-resources.bat %src% %unzipped%
+if "%use_tar%" == "true" (
+	call uncompress-resources.bat -t %src% %unzipped%
+) else (
+	call uncompress-resources.bat %src% %unzipped%
+)
 call align-and-sign.bat %unzipped% %dst%
